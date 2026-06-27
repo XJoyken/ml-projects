c19819274719a802871dd887b73fc9bf8b621dc1
XJoyken <daniel.kapchagay@gmail.com>
2026-06-27 22:19:09 +0500

    shell command updated

--- Titanic_Survival_Prediction/start_mlflow.sh
+++ Titanic_Survival_Prediction/start_mlflow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install mlflow psycopg2-binary
-mlflow server --backend-store-uri postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$MLFLOW_DB --default-artifact-root /data/mlruns --host 0.0.0.0 --port 5000 --allowed-hosts mlflow,mlflow:5000,localhost,localhost:5000,0.0.0.0
+mlflow server --backend-store-uri postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$MLFLOW_DB --artifacts-destination /data/mlruns --host 0.0.0.0 --port 5000 --serve-artifacts --allowed-hosts mlflow,mlflow:5000,localhost,localhost:5000,0.0.0.0
